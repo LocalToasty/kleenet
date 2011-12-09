b197dd7e5af138d2c7050fbec6852460d87cd924
Oscar Dustmann <oscar.dustmann@rwth-aachen.de>
2011-12-09 03:13:30 +0100

    sorry, forgot to add NetUserSearcher.h

---
+++ lib/KleeNet/NetUserSearcher.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <memory>
+#include "net/util/SharedPtr.h"
+#include "net/ClusterSearcherStrategy.h"
+
+namespace net {
+  class SearcherStrategy;
+}
+
+namespace kleenet {
+  namespace searcherautorun {
+    struct AF {
+      virtual ~AF() {}
+    };
+    class SearcherAutoRun {
+      private:
+        typedef std::auto_ptr<AF> F;
+        net::util::SharedPtr<net::SearcherStrategy> baseStrategy;
+        net::util::SharedPtr<net::SearcherStrategy> strategyAdapter;
+        F lockStep, cooja, clusterLockStep, clusterCooja;
+        SearcherAutoRun();
+      public:
+        static SearcherAutoRun const self;
+        void noop() const; // stupid stupid linker!
+    };
+  }
+}

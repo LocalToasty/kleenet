3516ed643499639ea91b9d67e05efaca867398ff
Oscar Dustmann <oscar.dustmann@rwth-aachen.de>
2012-03-08 14:21:12 +0100

    a bit docu

--- include/net/BasicState.h
+++ include/net/BasicState.h
@@ -7,6 +7,16 @@ namespace net {
   class StateDependantI;
   class RegisterChildDependant;
 
+  /* Fundamental type whose objects model logical states in the net-module.
+   *   Basic States have many *locical* members (e.g. MappingInformation) but these
+   *   are not found as actual members of this class as they are dynamic.
+   *   Instead we handle logical members in the StateDependant infrastructure.
+   *
+   *   The reason is automation: A basic state may have members defined loosely in
+   *   compilation units without chaing this file. Whenever a Basic state is duplicated
+   *   (i.e. via the copy-ctor) the StateDependant type will take care of cloning
+   *   all logical members, if they exist.
+   */
   class BasicState {
     friend class RegisterChildDependant;
     private:
